d7033d4f4dc7e22a21244200f42bb8e82b55ae88
Gregory Wakefield <gregory.k.wakefield@gmail.com>
2021-07-15 02:18:41 +0100

    Added justification and returns 2D array of layers

--- Bold text.jsx
+++ Bold text.jsx
@@ -1,47 +1,50 @@
-app.beginUndoGroup("Text with bold words");
-
-var comp = app.project.activeItem;
+var myComp = app.project.activeItem;
 
 var text = "*Lorem* ipsum dolor sit amet, consectetur adipiscing elit, sed *do eiusmod middle tempor* incididunt ut labore et dolore magna *aliqua.*";
 
-// var textOptions = {
-//     applyFill: false,
-//     applyStroke: false,
-//     fillColor: [1, 1, 1],
-//     font: "MyriadPro-Regular",
-//     fontSize: 60,
-//     justification: ParagraphJustification.LEFT_JUSTIFY,
-//     leading: 60 * 1.2, //fontSize * 1.2 = same as Auto
-//     strokeColor: [1, 0, 0],
-//     strokeOverFill: false,
-//     strokeWidth: 5,
-//     tracking: 0,
-// }
+var textOptions = {
+    applyFill: true,
+    applyStroke: true,
+    fillColor: [1, 1, 1],
+    font: "ArialMT",
+    fontSize: 60,
+    justification: ParagraphJustification.RIGHT_JUSTIFY,
+    leading: 60 * 1.2, //fontSize * 1.2 = same as Auto
+    strokeColor: [1, 0, 0],
+    strokeOverFill: false,
+    strokeWidth: 5,
+    tracking: 0,
+}
 
-var bold = "MyriadPro-Bold"
+var bold = "Arial-BoldMT"
 
-textWithBold(text, bold);
+textWithBold(myComp, text, bold, textOptions);
+
+
 
 /**
  * Text with bold words
  * -------------------
  * Makes point text layers with light or bold words.
  *
- * @param {string} text Text layer.
+ * @param {item object} comp Comp to add text to.
+ * @param {string} text Text string.
  * @param {string} boldFont Bold font type.
- * @param {object} textOptions Optional change character settings such as font, fontSize, colour etc (Only left justify currently supported)
+ * @param {object} textOptions Optional, change character settings such as font, fontSize, colour, justification etc (Full justify not supported, if selected will default to left justify)
  * @param {number} x Optional, x position for first row of text. Default 20.
  * @param {number} y Optional, y position for first row of text. Default middle of comp.
  * @param {number} w Optional, width of text before starting a new line. Default width of comp - 10.
  * @param {string} delimiter Optional, character used to note if word(s) should be bold. Default *
- * @return nothing
+ * @return {array} Returns a 2D array of text layers, first array for line second for words
 */
-function textWithBold(text, boldFont, textOptions, x, y, w, delimiter) {
+function textWithBold(comp, text, boldFont, textOptions, x, y, w, delimiter) {
+    app.beginUndoGroup("Text with bold words");
 
     var textProp, textDocument, textLayer;
     var isBold = 0;
     var newX = x;
-    
+    var lines = 0;
+    var textLayerArr = [[]];
 
     //Default Settings
     x = x ? x : 20;
@@ -51,23 +54,24 @@ function textWithBold(text, boldFont, textOptions, x, y, w, delimiter) {
     textOptions = textOptions ? textOptions : {};
 
     var regex = new RegExp("\\" + delimiter, "g");
-    
+
     //Create text layer to pull defaults from
     textLayer = newText("", x, y);
     textOptions = {
         applyFill: textOptions.applyFill == undefined ? textDocument.applyFill : textOptions.applyFill,
         applyStroke: textOptions.applyStroke == undefined ? textDocument.applyStroke : textOptions.applyStroke,
-        fillColor: textOptions.fillColor == undefined ? (textDocument.applyFill ? textDocument.fillColor : [0,0,0]) : textOptions.fillColor, //Even if applyFill/applyStroke is false fillColor/fillStroke expects an array.
+        fillColor: textOptions.fillColor == undefined ? (textDocument.applyFill ? textDocument.fillColor : [0, 0, 0]) : textOptions.fillColor, //Even if applyFill/applyStroke is false fillColor/fillStroke expects an array.
         font: textOptions.font == undefined ? textDocument.font : textOptions.font,
         fontSize: textOptions.fontSize == undefined ? textDocument.fontSize : textOptions.fontSize,
-        justification: textOptions.justification == undefined ? ParagraphJustification.LEFT_JUSTIFY : ParagraphJustification.LEFT_JUSTIFY, //Only left justify currently supported
+        justification: textOptions.justification == undefined ? textDocument.justification : textOptions.justification,
         leading: textOptions.leading == undefined ? textDocument.leading : textOptions.leading,
-        strokeColor: textOptions.strokeColor == undefined ? (textDocument.applyStroke ? textDocument.strokeColor : [0,0,0]) : textOptions.strokeColor,
+        strokeColor: textOptions.strokeColor == undefined ? (textDocument.applyStroke ? textDocument.strokeColor : [0, 0, 0]) : textOptions.strokeColor,
         strokeOverFill: textOptions.strokeOverFill == undefined ? textDocument.strokeOverFill : textOptions.strokeOverFill,
         strokeWidth: textOptions.strokeWidth == undefined ? textDocument.strokeWidth : textOptions.strokeWidth,
         tracking: textOptions.tracking == undefined ? textDocument.tracking : textOptions.tracking
     };
     textLayer.remove();
+    textLayerArr[lines].splice(textLayerArr[lines].length - 1)
 
     //Loop through words
     for (var i = 0; i < text.split(" ").length; i++) {
@@ -112,8 +116,8 @@ function textWithBold(text, boldFont, textOptions, x, y, w, delimiter) {
         newX = textLayer.position.value[0] + textLayer.sourceRectAtTime(0, false).left + textLayer.sourceRectAtTime(0, false).width;
 
         if (newX > x + w) {
-            removeWord();
             y = textLayer.position.value[1] + textOptions.leading;
+            removeWord();
             newText(word.replace(regex, ""), x, y)
             applyOptions()
             newX = textLayer.position.value[0] + textLayer.sourceRectAtTime(0, false).left + textLayer.sourceRectAtTime(0, false).width;
@@ -127,7 +131,9 @@ function textWithBold(text, boldFont, textOptions, x, y, w, delimiter) {
 
     }
     function newText(word, x, y) {
-        textLayer = comp.layers.addText(word);
+        textLayer = comp.layers.addText(word)
+        textLayerArr[lines].push(textLayer);
+        //textLayer = textLayerArr[lines][textLayerArr[lines].length-1];
         textLayer.position.setValue([x, y])
         textProp = textLayer.property("Source Text");
         textDocument = textProp.value;
@@ -143,6 +149,13 @@ function textWithBold(text, boldFont, textOptions, x, y, w, delimiter) {
         var lastIndex = textDocument.text.lastIndexOf(" ");
         textDocument.text = textDocument.text.substring(0, lastIndex);
         textProp.setValue(textDocument);
+
+        textLayerArr.push([]);
+        if (textLayer.sourceRectAtTime(0, false).width == 0) {
+            textLayerArr[lines].splice(textLayerArr[lines].length - 1);
+            textLayer.remove();
+        }
+        lines++;
     }
 
     function applyOptions(bold) {
@@ -155,7 +168,7 @@ function textWithBold(text, boldFont, textOptions, x, y, w, delimiter) {
         textDocument.applyStroke = textOptions.applyStroke;
         textDocument.fillColor = textOptions.fillColor;
         textDocument.fontSize = textOptions.fontSize;
-        textDocument.justification = textOptions.justification;
+        textDocument.justification = ParagraphJustification.LEFT_JUSTIFY;
         textDocument.leading = textOptions.leading;
         textDocument.strokeColor = textOptions.strokeColor;
         textDocument.strokeOverFill = textOptions.strokeOverFill;
@@ -163,6 +176,23 @@ function textWithBold(text, boldFont, textOptions, x, y, w, delimiter) {
         textDocument.tracking = textOptions.tracking;
         textProp.setValue(textDocument);
     }
-}
 
-app.endUndoGroup();
+    if (textOptions.justification == ParagraphJustification.CENTER_JUSTIFY || textOptions.justification == ParagraphJustification.RIGHT_JUSTIFY) {
+        for (var i = 0; i < textLayerArr.length; i++) {
+            var allWidth = 0;
+            for (var j = 0; j < textLayerArr[i].length; j++) {
+                allWidth += textLayerArr[i][j].sourceRectAtTime(0, false).left + textLayerArr[i][j].sourceRectAtTime(0, false).width;
+            }
+            var layerWidth = 0;
+            for (var j = 0; j < textLayerArr[i].length; j++) {
+                if (textOptions.justification == ParagraphJustification.CENTER_JUSTIFY) newX = (w / 2) - (allWidth / 2) + layerWidth + x;
+                if (textOptions.justification == ParagraphJustification.RIGHT_JUSTIFY) newX = w - allWidth + layerWidth + x;
+                layerWidth += textLayerArr[i][j].sourceRectAtTime(0, false).left + textLayerArr[i][j].sourceRectAtTime(0, false).width;
+                textLayerArr[i][j].position.setValue([newX, textLayerArr[i][j].position.value[1]])
+            }
+        }
+    }
+
+    app.endUndoGroup();
+    return textLayerArr;
+}
